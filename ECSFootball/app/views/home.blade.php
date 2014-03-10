c7de36200fe65631375109119f4deed1d0e29e0f
Ben <lumezious@googlemail.com>
2014-03-10 09:38:18 +0000

    Revert "Test"
    This reverts commit 0631a5456a9a88c9f22f63086055d3c960e3159e.

--- ECSFootball/app/views/home.blade.php
+++ ECSFootball/app/views/home.blade.php
@@ -10,7 +10,7 @@
 			<h3>Welcome to DVD Rental Heaven</h3>
 			<p>Here at Red Carpet Films we are dedicated to providing the best movies at exceptional prices. With thousands of titles to choose from, you will never be stuck for a movie to watch.</p>
 			<p>Customers experience a service that is second to none, with tracked next day delivery and no late fees, it's no wonder that we were voted top DVD rental provider in the UK for the past 10 years.</p>
-			<strong>Red Carpet Films - DVD heaven really igjhgjhghjgs a place on Earth</strong>
+			<strong>Red Carpet Films - DVD heaven really is a place on Earth</strong>
 
 			<hr />
 

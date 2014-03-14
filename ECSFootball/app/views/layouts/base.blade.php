a3a04e05932ea320afec3e1a65321ccc3678271e
Ben <lumezious@googlemail.com>
2014-03-14 14:35:35 +0000

    Updated nav and base template - Logo and menu headings

--- ECSFootball/app/views/layouts/base.blade.php
+++ ECSFootball/app/views/layouts/base.blade.php
@@ -7,15 +7,14 @@
 	{{ HTML::script('https://code.jquery.com/jquery.js') }}
 	{{ HTML::script('lib/bootstrap/js/bootstrap.min.js') }}
 	{{ HTML::script('js/holder.js') }}
-
 	<meta charset="UTF-8">
 	<title>@yield('title') | ECSS Football</title>
 </head>
 <body>
-	<div class="header-logo center">
-		<img src="/img/RedCarpetFilms.png" style="width:600px;"/>
-	</div>
 	<div class="container">
+		<div class="header-logo">
+			<img src="/img/ecss.png" style="width:150px;"/>
+		</div>
 		<div class="white-background">
 			@include('layouts.nav')
 
--- ECSFootball/app/views/layouts/nav.blade.php
+++ ECSFootball/app/views/layouts/nav.blade.php
@@ -10,25 +10,39 @@
 		</div>
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
-				<li><a href="{{ action('HomeController@showHomePage') }}">Home</a></li>
-				<li class="dropdown">
-					<a href="" class="dropdown-toggle" data-toggle="dropdown">Genres <b class="caret"></b></a>
-					<ul class="dropdown-menu">
-						<li><a href="">blah</a></li>
-						<li><a href="">blah</a></li>
-						<li><a href="">blah</a></li>
-					</ul>
-				</li>
-				<li><a href="">Film List</a></li>
+				<li><a href="{{ action('HomeController@showHomePage') }}">Latest News</a></li>
+				<li><a href="{{ action('HomeController@showHomePage') }}">Team</a></li>
+				<li><a href="{{ action('HomeController@showHomePage') }}">Fixtures &amp; Results</a></li>
+				<li><a href="{{ action('HomeController@showHomePage') }}">League Table</a></li>
+				<li><a href="{{ action('HomeController@showHomePage') }}">Contact Us</a></li>
 			</ul>
-			<form action="" method="post" class="navbar-form navbar-right" role="search">
-				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Search Films" name="search">
-				</div>
-				<button type="submit" class="btn btn-default">
-					<span class="glyphicon glyphicon-search"></span>
-				</button>
-		    </form>
+
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown" id="menu1">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#menu1">
+						Login
+						<b class="caret"></b>
+					</a>
+					<div class="dropdown-menu">
+						<form style="margin: 0px" accept-charset="UTF-8" action="/sessions" method="post">
+							<div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" />
+								<input name="authenticity_token" type="hidden" value="4L/A2ZMYkhTD3IiNDMTuB/fhPRvyCNGEsaZocUUpw40=" />
+							</div>
+							<fieldset class='textbox' style="padding:10px">
+								<input style="margin-top: 8px" type="text" placeholder="Username" />
+								<input style="margin-top: 8px" type="password" placeholder="Passsword" />
+								<input class="btn-primary" name="commit" type="submit" value="Log In" />
+							</fieldset>
+						</form>
+					</div>
+			   </li>
+			</ul>
+			<script language="javascript">
+			    $('.dropdown-toggle').dropdown();
+			    $('.dropdown-menu').find('form').click(function (e) {
+			        e.stopPropagation();
+			      });
+			</script>
 		</div>
 	</div>
 </div>

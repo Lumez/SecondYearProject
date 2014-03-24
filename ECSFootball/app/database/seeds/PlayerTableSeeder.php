1db838efb78c522b70d3ead8d6fca0ed064dfaab
Ben <lumezious@googlemail.com>
2014-03-24 13:16:14 +0000

    Added player database seeder to load in an admin account

---
+++ ECSFootball/app/database/seeds/PlayerTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+class PlayerTableSeeder extends Seeder
+{
+
+	public function run()
+	{
+		DB::table('player')->delete();
+		Player::create(array(
+			'first_name'     => 'Ben',
+			'last_name'     => 'Kneebone',
+			'email'    => 'bk8g11@soton.ac.uk',
+			'password' => Hash::make('awesome'),
+			'is_admin' => '1',
+			'about_me' => 'This an about me',
+			'facebook_URL' => 'bk8g11'
+		));
+	}
+
+}

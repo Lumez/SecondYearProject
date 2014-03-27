a5214f5a46bd8f53e0f1d12630da859b17ce2c60
gc1e12 <AARON-MAC@dhcp-204-169.wireless.soton.ac.uk>
2014-03-27 19:22:32 +0000

    5238319cd748ef5968a06984442306c157d9e85f
    Updated

--- ECSFootball/app/views/partials/sidebarpartial/weather.blade.php
+++ ECSFootball/app/views/partials/sidebarpartial/weather.blade.php
@@ -7,22 +7,18 @@
 		<b>Weather forecast</b>
 	</h4>
 
-	@foreach($weather_array as $day)
+	<div style = "text-align:left; padding-left:15px;">
+		@foreach($weather_array as $day)
 
-		<?php 
+			<?php 
+				$date = strtotime($day->date);
+				$myDate = date ('d-m-y', $date);
+			?>
+			
+			<p>  <b>{{ $myDate }} </b> --> {{ $day->weatherDesc[0]->value }} </p>
 
-			$date = strtotime($day->date);
-			$myDate = date ('d-m-y', $date);
-
-		?>
-		
-
-		<p>  {{ $myDate }} --> {{ $day->weatherDesc[0]->value }} </p>
-
-
-
-
-	@endforeach
+		@endforeach
+	</div>
 
 	Powered by <a href="http://www.worldweatheronline.com/" 
 	title="Free local weather content provider" 

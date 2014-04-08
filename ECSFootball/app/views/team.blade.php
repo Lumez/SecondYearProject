be109837983bd1733b2ee20d564437135b271db4
Stuart Kellaway <sk18g12@soton.ac.uk>
2014-04-08 22:37:21 +0100

    Header changed in Team Page

--- ECSFootball/app/views/team.blade.php
+++ ECSFootball/app/views/team.blade.php
@@ -13,7 +13,7 @@
 <div class="padded-content">
 	<div class="row">
 		<div class="col-md-8 center">
-			<h3>The Team</h3>
+			<h1>The Team</h1>
 			<p><b>Kit Colours:</b> Navy & Yellow</p>
 			<p>Customers experience a service that is second to none, with tracked next day delivery and no late fees, it's no wonder that we were voted top DVD rental provider in the UK for the past 10 years.</p>
 			<strong>Red Carpet Films - DVD heaven really is a place on Earth</strong>

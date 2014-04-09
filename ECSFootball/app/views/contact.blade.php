09a685828621b3d44cf80b48a2c81ef2cdb0765c
Stuart Kellaway <sk18g12@soton.ac.uk>
2014-04-09 01:08:32 +0100

    Contact Form created

--- ECSFootball/app/views/contact.blade.php
+++ ECSFootball/app/views/contact.blade.php
@@ -12,18 +12,24 @@
 
 <div class="padded-content">
 	<div class="row">
-		<div class="col-md-8 center">
+		<div class="col-md-8">
+		<div class="col-md-12 center">
 			<h1>Contact Us</h1>
 			<p>If you have any query about the team, please fill in the form below:</p>
-		<hr />
+			<hr />
+		</div>
 			<form role="form">
+			<div class="form-group">
+			    <label for="InputEmail1">Your Full Name</label>
+			    <input type="email" class="form-control" id="InputEmail1" placeholder="Enter Name">
+			  </div>
 			  <div class="form-group">
-			    <label for="InputEmail1">Your Email address</label>
-			    <input type="email" class="form-control" id="InputEmail1" placeholder="Enter email">
+			    <label for="InputEmail2">Your Email address</label>
+			    <input type="email" class="form-control" id="InputEmai2" placeholder="Enter E-mail">
 			  </div>
 			   <div class="form-group">
-			   <label for="InputEmail2">Your Message</label>
-			  <textarea class="form-control" id="InputEmail2" placeholder="Enter Message" rows="3"></textarea>
+			   <label for="InputEmail3">Your Message</label>
+			  <textarea class="form-control" id="InputEmail3" placeholder="Enter Message" rows="3"></textarea>
 			   </div>
 			 <button type="submit" class="btn btn-danger">Submit Message</button>
 			 <button type="reset" class="btn btn-info">Clear</button>

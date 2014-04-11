ebfd44669f8701c1b27c4964f47dbd5822ca1482
Eddikos <ea2g12@soton.ac.uk>
2014-04-11 21:42:21 +0100

    Update styling for User's Fixtures table

--- ECSFootball/app/views/fixture.blade.php
+++ ECSFootball/app/views/fixture.blade.php
@@ -15,73 +15,82 @@
 		<div class="col-md-8 center">
 			<h3>Fixture and Results</h3>
 			
+			<table class="table table-hover table-condensed fixtureTable">
+				<tr>
+					<th>Date</th>
+					<th>Score</th>
+					<th>Home Team</th>
+					<th></th>
+					<th>Away Team</th>
+					<th>Score</th>
+					<th>Match Profile</th>
+				</tr>
 			
 			@foreach ($fixtures as $fixture)
-			<div class="row">
-				<div class="col-md-3">
-					{{ $fixture->date }}
-				</div>
-				<div class="col-md-5 fixture">
-					@if ($fixture->is_home == 1)
-					
-						<div class="left">{{ $fixture->ecs_score }} |</div> 
-						<div class="center">ECSS vs {{ $fixture->against_team }}</div> 
-						<div class="right">| {{ $fixture->against_score }}</div>
-					@else 
-						<div class="left">{{ $fixture->against_score }} |</div>
-						<div class="center">{{ $fixture->against_team }} vs ECSS </div>
-						<div class="right">| {{ $fixture->ecs_score }}</div>
-					@endif
-				</div>
-				<div class="col-md-3">
-					<!-- Button trigger modal -->
-					<button class="" data-toggle="modal" data-target="#fixtureModal{{ $fixture->fixture_id }}">
-					  View Report
-					</button>
-					
-					<!-- Modal -->
-					<div class="modal fade" id="fixtureModal{{ $fixture->fixture_id }}" tabindex="-1" role="dialog" aria-labelledby="modalLabel{{ $fixture->fixture_id }}" aria-hidden="true">
-					  <div class="modal-dialog">
-						<div class="modal-content">
-						  <div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-						<h4 class="modal-title" id="modalLabel{{ $fixture->fixture_id }}">
-							<div class="fixtureTitle">
-								{{ $fixture->date }}  : &nbsp;
-								@if ($fixture->is_home)
-									{{ $fixture->ecs_score }} |
-									ECSS vs
-									{{ $fixture->against_team }} |
-									{{ $fixture->against_score }}
-								@else 
-									{{ $fixture->against_score }} |
-									{{ $fixture->against_team }}
-									vs ECSS |
-									{{ $fixture->ecs_score }}
-								@endif
-							</div>
-						</h4>
-						  </div>
-						  <div class="modal-body">
-						{{ $fixture->profile }}						
-						<hr/>
-						  </div>
-						  <div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
-						  </div>
-						</div>
-					  </div>
-					</div>
-				</div>
-				<div class="col-md-3">
-					
-					<!--<a href="{{ action('FixturesAndResultsController@deleteFixture') }}"><img id="deleteButton" src="/img/delete.png" width="20px"/></a>-->
-				</div>				
-			</div>
+	
+						<tr>					
+							@if ($fixture->is_home == 1)
+								<td>{{ date('d F Y',strtotime($fixture->date)); }}</td>
+								<td>{{ $fixture->ecs_score }}</td>
+								<td>ECSS</td>
+								<td>vs</td>
+								<td>{{ $fixture->against_team }}</td>
+								<td>{{ $fixture->against_score }}</td>
+							@else
+								<td>{{ date('d F Y',strtotime($fixture->date)); }}</td>
+								<td>{{ $fixture->against_score }}</td>
+								<td>{{ $fixture->against_team }}</td>
+								<td>vs</td>
+								<td>ECSS</td>
+								<td>{{ $fixture->ecs_score }}</td>
+							@endif
+								<td>
+									<!-- Button trigger modal -->
+									<button class="btn btn-default" data-toggle="modal" data-target="#fixtureModal{{ $fixture->fixture_id }}">
+										<span class="glyphicon glyphicon-comment"></span>
+									</button>
+									
+									<!-- Modal -->
+									<div class="modal fade" id="fixtureModal{{ $fixture->fixture_id }}" tabindex="-1" role="dialog" aria-labelledby="modalLabel{{ $fixture->fixture_id }}" aria-hidden="true">
+									  <div class="modal-dialog">
+										<div class="modal-content">
+										  <div class="modal-header">
+										<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+										<h4 class="modal-title" id="modalLabel{{ $fixture->fixture_id }}">
+											<div class="fixtureTitle">
+												{{ date('d F Y',strtotime($fixture->date)); }}  : &nbsp;
+												@if ($fixture->is_home)
+													{{ $fixture->ecs_score }} |
+													ECSS vs
+													{{ $fixture->against_team }} |
+													{{ $fixture->against_score }}
+												@else 
+													{{ $fixture->against_score }} |
+													{{ $fixture->against_team }}
+													vs ECSS |
+													{{ $fixture->ecs_score }}
+												@endif
+											</div>
+										</h4>
+										  </div>
+										  <div class="modal-body">
+										{{ $fixture->profile }}						
+										<hr/>
+										  </div>
+										  <div class="modal-footer">
+										<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+										<!--<button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span></button>-->
+										  </div>
+										</div>
+									  </div>
+									</div>	
+								</td>
+						</tr>
+						
 			@endforeach
-
-			<hr />
+			</table>
+			
+			<hr/><br/>
 			<div id="disqus_thread"></div>
 			<script type="text/javascript">
 				/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */

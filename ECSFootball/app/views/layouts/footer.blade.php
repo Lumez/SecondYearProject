f344e09801134792b7602e2c3b7b7bb8a349625d
Eddikos <ea2g12@soton.ac.uk>
2014-04-12 04:32:00 +0100

    Basic Styling of the Footer

--- ECSFootball/app/views/layouts/footer.blade.php
+++ ECSFootball/app/views/layouts/footer.blade.php
@@ -1,4 +1,8 @@
+<div class="navbar navbar-inverse navbar-static-top" role="navigation">
+	<div class="arrowLeft"></div><div class="arrowRight"></div>
+</div>
 <div class="footer">
 	<p>Copyright &copy; <?php echo(date('Y')); ?> ECSS</p>
 	<p>Developed by Damn Nizze Design</p>
+	<p><script type="text/javascript"> document.write("Page was last modified on: " + document.lastModified);</script></p>
 <div>

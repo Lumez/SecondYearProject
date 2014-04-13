b551619ff48802cc245be5727835e53def96915e
gc1e12 <AARON-MAC@AARONs-MacBook-Pro.local>
2014-04-13 18:05:36 +0100

    UPDATE weather sidebar with icon

--- ECSFootball/app/views/partials/sidebarpartial/weather.blade.php
+++ ECSFootball/app/views/partials/sidebarpartial/weather.blade.php
@@ -1,19 +1,35 @@
+{{ HTML::style('css/weather.style.css') }}
+
 <br/>
 <div class="sidebarItem"> 
 	<h4><b>Weather forecast</b></h4>
 	
 	<div style = "text-align:left; padding-left:15px;">
+
+	<br/>	
+	<hr/>
+
 		@foreach($weather_array as $day)
 
 			<?php 
 				$date = strtotime($day->date);
-				$myDate = date ('d-m-y', $date);
+				$myDate = date ('(D)d M ', $date);
 			?>
 			
-			<p>  <b>{{ $myDate }} </b> --> {{ $day->weatherDesc[0]->value }} </p>
+			<p>  
+				<b>{{ $myDate }}</b> --
+
+				<!-- Weather Icon Image -->
+				<img src= "{{ $day->weatherIconUrl[0]->value }}" alt= "{{ $day->weatherDesc[0]->value }}" id="weathericon"/>
+				{{ $day->weatherDesc[0]->value }} 
+
+			</p>
+
+			<hr/>
 
 		@endforeach
 	</div>
+	<br/>
 	
 	Powered by <a href="http://www.worldweatheronline.com/" 
 	title="Free local weather content provider" 

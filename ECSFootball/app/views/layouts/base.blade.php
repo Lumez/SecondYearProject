993a48a8f18234e48c9a0c7cf86f8b2ff4c40ea0
gc1e12 <AARON-MAC@dhcp-207-179.wireless.soton.ac.uk>
2014-04-15 18:30:56 +0100

    Attempt 7 : Remove HTML Validation error

--- ECSFootball/app/views/layouts/base.blade.php
+++ ECSFootball/app/views/layouts/base.blade.php
@@ -23,11 +23,13 @@
 <body>
 	<div class="container">
 		<div class="right">
-                        <a id="accessibility" class="btn btn-danger" href="javascript:(function(){d=document;lf=d.createElement('script');lf.type='text/javascript';lf.id='ToolbarStarter';lf.text='var%20StudyBarNoSandbox=true';d.getElementsByTagName('head')[0].appendChild(lf);jf=d.createElement('script');jf.src='https://core.atbar.org/atbar/en/latest/atbar.min.js';jf.type='text/javascript';jf.id='ToolBar';d.getElementsByTagName('head')[0].appendChild(jf);})()" target="" title="" id="atbar">Accessibility Tools</a>
-                </div>
+            <a class="btn btn-danger accessibility" href="javascript:(function(){d=document;lf=d.createElement('script');lf.type='text/javascript';lf.id='ToolbarStarter';lf.text='var%20StudyBarNoSandbox=true';d.getElementsByTagName('head')[0].appendChild(lf);jf=d.createElement('script');jf.src='https://core.atbar.org/atbar/en/latest/atbar.min.js';jf.type='text/javascript';jf.id='ToolBar';d.getElementsByTagName('head')[0].appendChild(jf);})()" target="" title="" id="atbar">Accessibility Tools</a>
+        </div>
+
 		<div class="header-logo">
 			<a href="{{ action('HomeController@showHomePage') }}"><img src="/img/ecss.png" alt="ECS-Football Society's Logo" style="width:150px;"/></a>
 		</div>
+		
 		<div class="white-background">
 			@include('layouts.nav')
 

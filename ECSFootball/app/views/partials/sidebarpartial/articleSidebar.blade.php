3477f9dfc3cdd06c94b915e220e35231383d176d
Ben <lumezious@googlemail.com>
2014-04-15 12:06:22 +0100

    Fixed bug I made in the article sidebar, oops :S

--- ECSFootball/app/views/partials/sidebarpartial/articleSidebar.blade.php
+++ ECSFootball/app/views/partials/sidebarpartial/articleSidebar.blade.php
@@ -7,12 +7,12 @@
 	
 			@foreach ($latestNews as $article)
 				<h5><b>{{ $article->title }}</b></h5>
-				<a href="{{ action('HomeController@showArticlesPage',  $article->article_id) }}"><img class="newsImage" src="{{ $article->picture_URL }}" width="90px;"></a>
+				<a href="{{ action('HomeController@showArticlePage',  $article->article_id) }}"><img class="newsImage" src="{{ $article->picture_URL }}" width="90px;"></a>
 				
 				<!--Is used to cut the description to certain amount of characters, not to exceed the div-->
 				<!--{{ $description = $article->description; }}
 				{{ $description = substr($description,0,100).'...'; }}-->
-				<p class="newsDesc">{{ $description }} <a href="{{ action('HomeController@showArticlesPage',  $article->article_id) }}"> Read More...</a></p>
+				<p class="newsDesc">{{ $description }} <a href="{{ action('HomeController@showArticlePage',  $article->article_id) }}"> Read More...</a></p>
 				<hr/>
 			@endforeach
 	<div class="center"><a href="{{ action('HomeController@showHomePage') }}"> View Latest News</a></div>

535426f9bddf85f4c6d5c225cbba7b681ddd8924
gc1e12 <AARON-MAC@dhcp-207-179.wireless.soton.ac.uk>
2014-04-17 14:36:50 +0100

    Update subscriber sidebar

--- ECSFootball/app/controllers/SubscribeController.php
+++ ECSFootball/app/controllers/SubscribeController.php
@@ -21,7 +21,7 @@ class SubscribeController extends BaseController {
 		if($validator->passes()) {
 
 			$subscriber = new Subscriber;
-			$subscriber->email = Input::get('subscriberEmail');
+			$subscriber->email = Input::get('email');
 			$subscriber->save();
 
 			Mail::send('emails.subscribed', array(), function($message) use ($subscriber)
